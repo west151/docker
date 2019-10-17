070143bb17f08759d63c574ec9e2ac4cd334793f
gis1501 <gis1501@gmail.com>
2019-10-17 17:56:03 +0000

    update

--- raspberry/run-build.sh
+++ raspberry/run-build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo "Run build yocto poky."
+CL_GREEN="\033[0;32m"
+CL_YELLOY="\033[0;33m"
+CL_NC="\033[0m"
+
+echo -e "${CL_GREEN}Run build yocto poky.${CL_NC}"
+echo
 
 YOCTO_BRANCH="warrior"
 QT_VER="5.13"
 
 if ! [ -d $(pwd)/poky ];
 then
-    echo 'Clone poky'
+    echo -e "${CL_YELLOY}Clone poky${CL_NC}"
+    echo
     git clone -b $YOCTO_BRANCH git://git.yoctoproject.org/poky.git poky
     cd poky
     git clone -b $YOCTO_BRANCH https://github.com/west151/meta-snake
@@ -15,27 +21,31 @@ then
     git clone -b $YOCTO_BRANCH git://git.yoctoproject.org/meta-raspberrypi
     git clone -b $QT_VER git://code.qt.io/yocto/meta-qt5.git
 else
-    echo 'Update poky:' $YOCTO_BRANCH
+    echo -e "${CL_YELLOY}Update poky:${CL_NC}" $YOCTO_BRANCH
     cd $(pwd)/poky
     git fetch
     git pull origin
     #
-    echo 'Update meta-snake' $YOCTO_BRANCH
+    echo
+    echo -e "${CL_YELLOY}Update meta-snake:${CL_NC}" $YOCTO_BRANCH
     cd meta-snake
     git fetch
     git pull origin
     #
-    echo 'Update meta-openembedded:' $YOCTO_BRANCH
+    echo
+    echo -e "${CL_YELLOY}Update meta-openembedded:${CL_NC}" $YOCTO_BRANCH
     cd ../meta-openembedded
     git fetch
     git pull origin
     #
-    echo 'Update meta-raspberrypi' $YOCTO_BRANCH
+    echo
+    echo -e "${CL_YELLOY}Update meta-raspberrypi:${CL_NC}" $YOCTO_BRANCH
     cd ../meta-raspberrypi
     git fetch
     git pull origin
     #
-    echo 'Update meta-qt5:' $QT_VER
+    echo
+    echo -e "${CL_YELLOY}Update meta-qt5:${CL_NC}" $QT_VER
     cd ../meta-qt5
     git fetch
     git pull origin
@@ -63,3 +73,5 @@ docker build --no-cache . --tag snake-wboard-image:latest
 
 # run docker
 docker run --user=user:user -it --rm -v $PWD/build:/home/user/build snake-wboard-image:latest
+
+exit 0
