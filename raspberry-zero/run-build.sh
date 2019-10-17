12c719cc2acd4dca89e91a583109a77ad33d3f15
gis1501 <gis1501@gmail.com>
2019-10-17 17:59:16 +0000

    update

--- raspberry-zero/run-build.sh
+++ raspberry-zero/run-build.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
-echo "Run build yocto poky."
+CL_GREEN="\033[0;32m"
+CL_YELLOY="\033[0;33m"
+CL_NC="\033[0m"
+
+NAME_IMAGE="snake-zboard-image"
+NAME_SDK="meta-toolchain-qt5"
+
+echo
+echo -e "${CL_GREEN}Run build yocto poky.${CL_NC}"
+echo
 
 YOCTO_BRANCH="warrior"
 QT_VER="5.13"
 
 if ! [ -d $(pwd)/poky ];
 then
-    echo 'Clone poky'
+    echo -e "${CL_YELLOY}Clone poky${CL_NC}"
+    echo
     git clone -b $YOCTO_BRANCH git://git.yoctoproject.org/poky.git poky
     cd poky
-    #git clone -b $YOCTO_BRANCH https://github.com/west151/meta-snake-zero
     git clone -b master https://github.com/west151/meta-snake-zero
     git clone -b $YOCTO_BRANCH git://git.openembedded.org/meta-openembedded
     git clone -b $YOCTO_BRANCH git://git.yoctoproject.org/meta-raspberrypi
     git clone -b $QT_VER git://code.qt.io/yocto/meta-qt5.git
 else
-    echo 'Update poky:' $YOCTO_BRANCH
+    echo -e "${CL_YELLOY}Update poky:${CL_NC}" $YOCTO_BRANCH
     cd $(pwd)/poky
     git fetch
     git pull origin
     #
-    echo 'Update meta-snake-zero' $YOCTO_BRANCH
+    echo
+    echo -e "${CL_YELLOY}Update meta-snake-zero:${CL_NC}" $YOCTO_BRANCH
     cd meta-snake-zero
     git fetch
     git pull origin
     #
-    echo 'Update meta-openembedded:' $YOCTO_BRANCH
+    echo
+    echo -e "${CL_YELLOY}Update meta-openembedded:${CL_NC}" $YOCTO_BRANCH
     cd ../meta-openembedded
     git fetch
     git pull origin
     #
-    echo 'Update meta-raspberrypi' $YOCTO_BRANCH
+    echo
+    echo -e "${CL_YELLOY}Update meta-raspberrypi:${CL_NC}" $YOCTO_BRANCH
     cd ../meta-raspberrypi
     git fetch
     git pull origin
     #
-    echo 'Update meta-qt5:' $QT_VER
+    echo
+    echo -e "${CL_YELLOY}Update meta-qt5:${CL_NC}" $QT_VER
     cd ../meta-qt5
     git fetch
     git pull origin
@@ -59,8 +72,13 @@ docker system prune -f
 docker image prune -f
 
 # build docker image
-#docker build . --tag snake-zboard-image:latest
+docker build . --tag snake-zboard-image:latest
 docker build --no-cache . --tag snake-zboard-image:latest
 
 # run docker
-docker run --user=user:user -it --rm -v $PWD/build:/home/user/build snake-zboard-image:latest
+docker run --user=user:user -it --rm \
+--env YOCTO_IMAGE=$NAME_IMAGE \
+--env YOCTO_SDK=$NAME_SDK \
+-v $PWD/build:/home/user/build snake-zboard-image:latest
+
+exit 0
