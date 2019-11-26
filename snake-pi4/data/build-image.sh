d7595cad7e33723037b3c4d6d9456a75b7d8f911
gis1501 <gis1501@gmail.com>
2019-11-26 17:51:24 +0000

    update & new

---
+++ snake-pi4/data/build-image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CL_GREEN="\033[0;32m"
+CL_YELLOY="\033[0;33m"
+CL_NC="\033[0m"
+
+NAME_IMAGE="console-image"
+NAME_SDK="meta-toolchain-qt5"
+MACHINE="raspberrypi4"
+
+echo
+echo -e "${CL_GREEN}Run build yocto poky.${CL_NC}"
+
+# run docker
+echo
+echo -e "${CL_GREEN} Run docker${CL_NC}"
+echo
+
+#echo $PWD
+
+docker run --user=user:user -it --rm \
+--env YOCTO_MACHINE=$MACHINE \
+--env YOCTO_IMAGE=$NAME_IMAGE \
+--env YOCTO_SDK=$NAME_SDK \
+-v $PWD/yocto-data:/home/user/yocto-data snake-wboard-image:latest
+
+exit 0
--- snake-pi4/data/run-update.sh
+++ snake-pi4/data/run-update.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+CL_RED="\033[0;31m"
 CL_GREEN="\033[0;32m"
 CL_YELLOY="\033[0;33m"
 CL_NC="\033[0m"
 
-NAME_IMAGE="snake-wboard-image"
-NAME_SDK="meta-toolchain-qt5"
-YOCTO_BRANCH="warrior"
-QT_VER="5.13"
-MACHINE="raspberrypi-cm3"
-#raspberrypi-cm3 raspberrypi4
+NAME_IMAGE=$(awk -F"=" '/NAME_IMAGE/ {print ($2)}' image.conf)
+NAME_SDK=$(awk -F"=" '/NAME_SDK/ {print ($2)}' image.conf)
+YOCTO_BRANCH=$(awk -F"=" '/YOCTO_BRANCH/ {print ($2)}' image.conf)
+QT_VER=$(awk -F"=" '/QT_VER/ {print ($2)}' image.conf)
+MACHINE=$(awk -F"=" '/MACHINE/ {print ($2)}' image.conf)
+
+echo 
+echo -e "${CL_RED}NAME_IMAGE:${CL_NC}" $NAME_IMAGE
+echo -e "${CL_RED}NAME_SDK:${CL_NC}" $NAME_SDK
+echo -e "${CL_RED}YOCTO_BRANCH:${CL_NC}" $YOCTO_BRANCH
+echo -e "${CL_RED}QT_VER:${CL_NC}" $QT_VER
+echo -e "${CL_RED}MACHINE:${CL_NC}" $MACHINE
 
 echo
 echo -e "${CL_GREEN}Run build yocto poky.${CL_NC}"
@@ -30,11 +37,17 @@ if ! [ -d $(pwd)/poky ];
    echo -e "${CL_YELLOY}Clone poky${CL_NC}"
    echo
    git clone -b $YOCTO_BRANCH git://git.yoctoproject.org/poky.git poky
+   echo
    cd poky
-   git clone -b $YOCTO_BRANCH https://github.com/west151/meta-snake
+   git clone -b $YOCTO_BRANCH https://github.com/west151/meta-snake-pi4
+   echo
    git clone -b $YOCTO_BRANCH git://git.openembedded.org/meta-openembedded
+   echo
    git clone -b $YOCTO_BRANCH git://git.yoctoproject.org/meta-raspberrypi
+   echo
    git clone -b $QT_VER git://code.qt.io/yocto/meta-qt5.git
+   #
+   cd ../
  else
    echo -e "${CL_YELLOY}Update poky:${CL_NC}" $YOCTO_BRANCH
    cd poky
@@ -42,8 +55,8 @@ if ! [ -d $(pwd)/poky ];
    git pull origin
    #
    echo
-   echo -e "${CL_YELLOY}Update meta-snake:${CL_NC}" $YOCTO_BRANCH
-   cd meta-snake
+   echo -e "${CL_YELLOY}Update meta-snake-pi4:${CL_NC}" $YOCTO_BRANCH
+   cd meta-snake-pi4
    git fetch
    git pull origin
    #
@@ -64,10 +77,11 @@ if ! [ -d $(pwd)/poky ];
    cd ../meta-qt5
    git fetch
    git pull origin
+   #
+   cd ../../
 fi
 
 # создаем папку "build" для совместного использования с контенером 
-cd ../../
 if ! [ -d $(pwd)/build ];
  then
    # mkdir build
@@ -76,36 +90,7 @@ if ! [ -d $(pwd)/build ];
 fi
 
 # копируем конфигурационные файлы
-cp poky/meta-snake/conf/bblayers.conf.docker build/conf/bblayers.conf
-cp poky/meta-snake/conf/local.conf.example build/conf/local.conf
-
-#echo 'MACHINE = $MACHINE' >> build/conf/local.conf
-
-echo
-echo -e "${CL_GREEN} 2. Docker prune${CL_NC}"
-echo
-#docker system prune -f
-#docker image prune -f
-
-cd ../
-
-# build docker image
-echo
-echo -e "${CL_GREEN} 3. Build docker image${CL_NC}"
-echo
-#docker build --no-cache . --tag snake-wboard-image:latest
-
-# run docker
-echo
-echo -e "${CL_GREEN} 4. Run docker${CL_NC}"
-echo
-
-echo $PWD
-
-docker run --user=user:user -it --rm \
---env YOCTO_MACHINE=$MACHINE \
---env YOCTO_IMAGE=$NAME_IMAGE \
---env YOCTO_SDK=$NAME_SDK \
--v $PWD/yocto-data:/home/user/yocto-data snake-wboard-image:latest
+cp poky/meta-snake-pi4/conf/bblayers.conf.docker build/conf/bblayers.conf
+cp poky/meta-snake-pi4/conf/local.conf.example build/conf/local.conf
 
 exit 0
