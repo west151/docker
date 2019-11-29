396b00963ae0db98ed561bf7e2273b95306fc8d6
gis1501 <gis1501@gmail.com>
2019-11-29 13:10:41 +0000

    add: snake-xu4

---
+++ snake-xu4/run-update.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+CL_RED="\033[0;31m"
+CL_GREEN="\033[0;32m"
+CL_YELLOY="\033[0;33m"
+CL_NC="\033[0m"
+
+YOCTO_BRANCH=$(awk -F"=" '/YOCTO_BRANCH/ {print ($2)}' image.conf)
+QT_VER=$(awk -F"=" '/QT_VER/ {print ($2)}' image.conf)
+
+echo
+echo -e "${CL_RED}YOCTO_BRANCH:${CL_NC}" $YOCTO_BRANCH
+echo -e "${CL_RED}QT_VER:${CL_NC}" $QT_VER
+echo
+
+if ! [ -d $(pwd)/yocto-data ];
+then
+    mkdir yocto-data
+fi
+
+cd $(pwd)/yocto-data
+
+echo -e "${CL_GREEN}git clone or update.${CL_NC}"
+echo
+
+if ! [ -d $(pwd)/poky ];
+ then
+   echo -e "${CL_YELLOY}clone poky${CL_NC}"
+   echo
+   git clone -b $YOCTO_BRANCH git://git.yoctoproject.org/poky.git poky
+   echo
+   cd poky
+   git clone -b $YOCTO_BRANCH https://github.com/west151/meta-snake-xu4
+   echo
+   git clone -b $YOCTO_BRANCH git://git.openembedded.org/meta-openembedded
+   echo
+   git clone -b $YOCTO_BRANCH https://github.com/akuster/meta-odroid
+   echo
+   git clone -b $QT_VER git://code.qt.io/yocto/meta-qt5.git
+   #
+   cd ../
+ else
+   echo -e "${CL_YELLOY}update poky:${CL_NC}" $YOCTO_BRANCH
+   cd poky
+   git fetch && git pull origin
+   #
+   echo
+   echo -e "${CL_YELLOY}update meta-snake-xu4:${CL_NC}" $YOCTO_BRANCH
+   cd meta-snake-xu4
+   git fetch && git pull origin
+   #
+   echo
+   echo -e "${CL_YELLOY}update meta-openembedded:${CL_NC}" $YOCTO_BRANCH
+   cd ../meta-openembedded
+   git fetch && git pull origin
+   #
+   echo
+   echo -e "${CL_YELLOY}update meta-odroid:${CL_NC}" $YOCTO_BRANCH
+   cd ../meta-odroid
+   git fetch && git pull origin
+   #
+   echo
+   echo -e "${CL_YELLOY}update meta-qt5:${CL_NC}" $QT_VER
+   cd ../meta-qt5
+   git fetch && git pull origin
+   #
+   cd ../../
+fi
+
+# создаем папку "build" для совместного использования с контенером 
+if ! [ -d $(pwd)/build ];
+ then
+   # mkdir build
+   mkdir build
+   mkdir -p build/conf
+fi
+
+# копируем конфигурационные файлы
+cp poky/meta-snake-xu4/conf/bblayers.conf.docker build/conf/bblayers.conf
+cp poky/meta-snake-xu4/conf/local.conf.example build/conf/local.conf
+
+exit 0
