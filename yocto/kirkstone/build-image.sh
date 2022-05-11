f23ac8ac66fc6bc0786785363d85cb795fe79ec1
gis1501 <gis1501@gmail.com>
2022-05-11 17:47:07 +0000

    update: add: new file kirkstone

--- yocto/kirkstone/build-image.sh
+++ yocto/kirkstone/build-image.sh
@@ -6,13 +6,13 @@ CL_NC="\033[0m"
 
 WORK_DIR=$(pwd)
 NAME_IMAGE=$(awk -F"=" '/NAME_IMAGE/ {print ($2)}' image.conf)
+NAME_SDK=$(awk -F"=" '/NAME_SDK/ {print ($2)}' image.conf)
 MACHINE=$(awk -F"=" '/MACHINE/ {print ($2)}' image.conf)
 TARGET_DEPLOY=$(awk -F"=" '/TARGET_DEPLOY/ {print ($2)}' image.conf)
 YOCTO_BRANCH=$(awk -F"=" '/YOCTO_BRANCH/ {print ($2)}' image.conf)
 QT_VER=$(awk -F"=" '/QT_VER/ {print ($2)}' image.conf)
-TARGET_SUBDIR=worm-pi4/${YOCTO_BRANCH}/qt-${QT_VER}
-
-
+TARGET_SUBDIR=snake-cm4-qt6/${YOCTO_BRANCH}/qt-${QT_VER}
+YOCTO_IMAGE_DOCKER=$(awk -F"=" '/YOCTO_IMAGE_DOCKER/ {print ($2)}' image.conf)
 time=$(date +%s)
 
 echo
@@ -26,7 +26,8 @@ echo
 docker run --user=user:user -it --rm \
 --env YOCTO_MACHINE=$MACHINE \
 --env YOCTO_IMAGE=$NAME_IMAGE \
--v $PWD/yocto-data:/home/user/yocto-data yocto-image-v4:latest
+--env YOCTO_SDK=$NAME_SDK \
+-v $PWD/yocto-data:/home/user/yocto-data $YOCTO_IMAGE_DOCKER
 
 echo -e "${CL_GREEN}Deploy image: ${CL_NC}"
 echo
@@ -36,7 +37,11 @@ if [ -d ${TARGET_DEPLOY} ]; then
    mkdir -p ${TARGET_DEPLOY}/${TARGET_SUBDIR}/$(date +%Y%m%d)
    #
    echo -e "${CL_GREEN}Copy image: ${CL_NC}"
-   rsync --progress ${WORK_DIR}/yocto-data/build/tmp/deploy/images/${MACHINE}/*rootfs.rpi-sdimg ${TARGET_DEPLOY}/${TARGET_SUBDIR}/$(date +%Y%m%d)/
+   rsync --progress ${WORK_DIR}/yocto-data/build/tmp/deploy/images/raspberrypi4-64/*rootfs.rpi-sdimg ${TARGET_DEPLOY}/${TARGET_SUBDIR}/$(date +%Y%m%d)/
+   #
+   echo
+   echo -e "${CL_GREEN}Copy sdk: ${CL_NC}"
+   rsync --progress ${WORK_DIR}/yocto-data/build/tmp/deploy/sdk/*.sh ${TARGET_DEPLOY}/${TARGET_SUBDIR}/$(date +%Y%m%d)/
 fi
 
 secs=$(($(date +%s)-$time))
